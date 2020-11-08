1eab4546bfb5c5476af1b1da51a785281d87e682
Abigail Marticio <marticiobgl@gmail.com>
2020-11-08 23:47:31 +0800

    chore: add method compute_series

--- taylor-series/TaylorSeries.py
+++ taylor-series/TaylorSeries.py
@@ -3,3 +3,12 @@ class TaylorSeries(object):
         self.num_terms = num_terms
 
 class Sine(TaylorSeries):
+    def compute_series(self, x: float) -> float:
+        x = math.radians(x)
+        approximation = 0
+        for index in range(self.num_terms):
+            coefficient = ((-1) ** index)
+            numerator = x ** (2 * index + 1)
+            denominator = math.factorial(2 * index + 1)
+            approximation += (coefficient * (numerator / denominator))
+        return approximation
