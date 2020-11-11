120d3ab4352cfc2f6ad408cba20ea2c6ee50c42d
Abigail Marticio <marticiobgl@gmail.com>
2020-11-11 22:03:23 +0800

    chore: add compute_series method

---
+++ taylor/TaylorSeries.py
@@ -0,0 +1,43 @@
+import math
+
+
+class TaylorSeries(object):
+    def __init__(self, num_terms: int):
+        self.num_terms = num_terms
+
+    def compute_series(self, x: float) -> float:
+        raise NotImplementedError
+
+
+class Cosine(TaylorSeries):
+    def compute_series(self, x: float) -> float:
+        x = math.radians(x)
+        approximation = 0
+        for index in range(self.num_terms):
+            coefficient = (-1) ** index
+            numerator = x ** (2 * index)
+            denominator = math.factorial(2 * index)
+            approximation += coefficient * (numerator / denominator)
+        return approximation
+
+
+class Exponential(TaylorSeries):
+    def compute_series(self, x: float) -> float:
+        approximation = 0
+        for index in range(self.num_terms):
+            numerator = x ** index
+            denominator = math.factorial(index)
+            approximation += numerator / denominator
+        return approximation
+
+
+class Sine(TaylorSeries):
+    def compute_series(self, x: float) -> float:
+        x = math.radians(x)
+        approximation = 0
+        for index in range(self.num_terms):
+            coefficient = (-1) ** index
+            numerator = x ** (2 * index + 1)
+            denominator = math.factorial(2 * index + 1)
+            approximation += coefficient * (numerator / denominator)
+        return approximation
