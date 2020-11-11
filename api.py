74b17a2b489481954cde322b25f060ed354b4ca3
Abigail Marticio <marticiobgl@gmail.com>
2020-11-11 22:07:32 +0800

    chore: add view function and get request for exp

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from taylor import Cosine
+from taylor import Cosine, Exponential
 
 app = Flask(__name__)
 
@@ -9,6 +9,13 @@ def taylor_cosine():
     num_terms = request.args.get("num_terms", default=None, type=int)
     cos = Cosine(num_terms)
     return f"cos({num_terms}) = {cos(num_terms)}"
+
+
+@app.route("/taylor-series/exponential", methods=["GET"])
+def taylor_exponential():
+    num_terms = request.args.get("num_terms", default=None, type=int)
+    exp = Exponential(num_terms)
+    return f"exp({num_terms}) = {exp(num_terms)}"
 
 
 if __name__ == "__main__":
