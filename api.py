5e8524c6a1fbf73ac23eb56ecaa963eb898cf870
Abigail Marticio <68144906+abimarticio@users.noreply.github.com>
2021-01-02 21:10:19 +0800

    fix: add value param

--- api.py
+++ api.py
@@ -7,22 +7,25 @@ app = Flask(__name__)
 @app.route("/taylor-series/cosine", methods=["GET"])
 def taylor_cosine():
     num_terms = request.args.get("num_terms", default=None, type=int)
+    value = request.args.get("value", default=None, type=int)
     cos = Cosine(num_terms)
-    return f"cos({num_terms}) = {cos(num_terms)}"
+    return f"cos({value}) = {cos(value)}"
 
 
 @app.route("/taylor-series/exponential", methods=["GET"])
 def taylor_exponential():
     num_terms = request.args.get("num_terms", default=None, type=int)
+    value = request.args.get("value", default=None, type=int)
     exp = Exponential(num_terms)
-    return f"exp({num_terms}) = {exp(num_terms)}"
+    return f"exp({value}) = {exp(value)}"
 
 
 @app.route("/taylor-series/sine", methods=["GET"])
 def taylor_sine():
     num_terms = request.args.get("num_terms", default=None, type=int)
+    value = request.args.get("value", default=None, type=int)
     sin = Sine(num_terms)
-    return f"sin({num_terms}) = {sin(num_terms)}"
+    return f"sin({value}) = {sin(value)}"
 
 
 if __name__ == "__main__":
